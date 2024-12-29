6f721c798831a84f530837cca59ee2aca494669d
Sanjay <sanjaykumar.pushadapu@gmail.com>
2024-12-29 09:46:23 +0530

    removed unused methods

--- src/redux/posts/postsSlice.js
+++ src/redux/posts/postsSlice.js
@@ -16,42 +16,20 @@ const postSlice = createSlice({
   extraReducers: (builder) => {
     // Handle fetchPosts
     builder
-      // Handle when fetchPosts is pending
-      .addCase(fetchPosts.pending, () => {
-        // We don't need to track loading here anymore as it's handled globally.
-        // (The loading state is now in asyncSlice.js, managed globally)
-      })
+
       // Handle when fetchPosts is fulfilled
       .addCase(fetchPosts.fulfilled, (state, action) => {
         state.posts = action.payload; // Update posts with fetched data
       })
       // Handle when fetchPosts is rejected
-      .addCase(fetchPosts.rejected, () => {
-        // Error handling is done globally by asyncSlice.js
-        // If we wanted, we could dispatch custom actions for errors here as well.
-      })
 
-      // Handle createPost
-      .addCase(createPost.pending, () => {
-        // We don't need to track loading here anymore as it's handled globally.
-      })
       .addCase(createPost.fulfilled, (state, action) => {
         state.posts.push(action.payload); // Add the new post to the posts array
         state.createdPost = action.payload; // Optionally store the created post
       })
-      .addCase(createPost.rejected, () => {
-        // Handle error if createPost fails
-      })
 
-      // Handle deletePost
-      .addCase(deletePost.pending, () => {
-        // We don't need to track loading here anymore as it's handled globally.
-      })
       .addCase(deletePost.fulfilled, (state, action) => {
         state.posts = state.posts.filter((post) => post.id !== action.payload); // Remove the deleted post from the array
-      })
-      .addCase(deletePost.rejected, () => {
-        // Handle error if deletePost fails
       });
   },
 });
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
+// store/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import postReducer from './posts/postsSlice';
-import asyncReducer from './asyncSlice'; // Import the async slice
+import asyncReducer from './asyncSlice'; // Import asyncSlice for loading/error states
+
 const store = configureStore({
   reducer: {
     posts: postReducer,
-    async: asyncReducer, // Add async slice to the root reducer
+    async: asyncReducer, // Add async slice to the root reducer for global async state
   },
 });
 
